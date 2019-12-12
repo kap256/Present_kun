b7df28de6fd9c170b8561117604cb496db5ba427
カレプリ <kojikyo256@gmail.com>
2019-12-12 19:37:20 +0900

    自分のプレゼントが自分に返ってこないように振り分けるプレゼント交換抽選プログラム。

--- Present_kun/Form1.Designer.cs
+++ Present_kun/Form1.Designer.cs
@@ -27,13 +27,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.list_Names = new System.Windows.Forms.ListBox();
+            this.label_Result = new System.Windows.Forms.Label();
+            this.text_AddName = new System.Windows.Forms.TextBox();
+            this.button_AddName = new System.Windows.Forms.Button();
+            this.button_Result = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list_Names
+            // 
+            this.list_Names.FormattingEnabled = true;
+            this.list_Names.ItemHeight = 21;
+            this.list_Names.Location = new System.Drawing.Point(13, 13);
+            this.list_Names.Name = "list_Names";
+            this.list_Names.Size = new System.Drawing.Size(424, 403);
+            this.list_Names.TabIndex = 0;
+            this.list_Names.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.list_Names_MouseDoubleClick);
+            // 
+            // label_Result
+            // 
+            this.label_Result.AutoSize = true;
+            this.label_Result.Location = new System.Drawing.Point(444, 13);
+            this.label_Result.Name = "label_Result";
+            this.label_Result.Size = new System.Drawing.Size(0, 21);
+            this.label_Result.TabIndex = 1;
+            // 
+            // text_AddName
+            // 
+            this.text_AddName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.text_AddName.Location = new System.Drawing.Point(13, 441);
+            this.text_AddName.Name = "text_AddName";
+            this.text_AddName.Size = new System.Drawing.Size(332, 28);
+            this.text_AddName.TabIndex = 2;
+            // 
+            // button_AddName
+            // 
+            this.button_AddName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_AddName.Location = new System.Drawing.Point(351, 435);
+            this.button_AddName.Name = "button_AddName";
+            this.button_AddName.Size = new System.Drawing.Size(92, 38);
+            this.button_AddName.TabIndex = 3;
+            this.button_AddName.Text = "追加";
+            this.button_AddName.UseVisualStyleBackColor = true;
+            this.button_AddName.Click += new System.EventHandler(this.button_AddName_Click);
+            // 
+            // button_Result
+            // 
+            this.button_Result.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Result.Location = new System.Drawing.Point(807, 435);
+            this.button_Result.Name = "button_Result";
+            this.button_Result.Size = new System.Drawing.Size(92, 38);
+            this.button_Result.TabIndex = 4;
+            this.button_Result.Text = "抽選";
+            this.button_Result.UseVisualStyleBackColor = true;
+            this.button_Result.Click += new System.EventHandler(this.button_Result_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button_AddName;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(906, 486);
+            this.Controls.Add(this.button_Result);
+            this.Controls.Add(this.button_AddName);
+            this.Controls.Add(this.text_AddName);
+            this.Controls.Add(this.label_Result);
+            this.Controls.Add(this.list_Names);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "プレゼントくん";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox list_Names;
+        private System.Windows.Forms.Label label_Result;
+        private System.Windows.Forms.TextBox text_AddName;
+        private System.Windows.Forms.Button button_AddName;
+        private System.Windows.Forms.Button button_Result;
     }
 }
 
--- Present_kun/Form1.cs
+++ Present_kun/Form1.cs
@@ -12,9 +12,94 @@ namespace Present_kun
 {
     public partial class Form1 : Form
     {
+        int ResultId = -1;
+        List<int> Results = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        //リストに名前を追加します。
+        private void button_AddName_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(text_AddName.Text)) {
+                list_Names.Items.Add(text_AddName.Text);
+                text_AddName.Text = "";
+            }
+        }
+
+        //リストから名前を削除します。
+        private void list_Names_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            list_Names.Items.Remove(list_Names.SelectedItem);
+        }
+
+        //結果表示をします。
+        private void button_Result_Click(object sender, EventArgs e)
+        {
+            //初回実行時
+            if (Results == null) {
+                if (list_Names.Items.Count < 2) {
+                    return;
+                }
+                list_Names.Enabled = false;
+                text_AddName.Enabled = false;
+                button_AddName.Enabled = false;
+                label_Result.Text = "";
+                AcceptButton = button_Result;
+
+                CreateNewResult();
+            }
+
+            //結果の順次表示
+            label_Result.Text += 
+                list_Names.Items[ResultId] 
+                + " => "
+                + list_Names.Items[Results[ResultId]]
+                + Environment.NewLine;
+
+            ResultId++;
+
+
+            //最後まで表示が終わった時
+            if (ResultId>= Results.Count) {
+                list_Names.Enabled = true;
+                text_AddName.Enabled = true;
+                button_AddName.Enabled = true;
+                AcceptButton = button_AddName;
+
+                Results = null;
+            }
+
+        }
+
+        //結果作成
+        private void CreateNewResult()
+        {
+            var rand = new Random();
+
+            do {
+                Results = new List<int>();
+                var values = new List<int>();
+                for (int i =0;i< list_Names.Items.Count; i++) {
+                    values.Add(i);
+                }
+                
+                while(values.Count>0) {
+                    int index = rand.Next(values.Count);
+                    int val = values[index];
+                    values.RemoveAt(index);
+                    Results.Add(val);
+                    if(Results.Count == val +1) {  //完全順列じゃなければやり直し
+                        Results = null;
+                        break;
+                    }
+                }
+            } while (Results==null);
+
+            ResultId = 0;
+        }
+
     }
 }
